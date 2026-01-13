77460a908449b62c26bb10e5b7a8fbd50211bcb1
shweta-thopate <thopateshweta20@gmail.com>
2026-01-13 18:58:12 +0530

    Create Aggrigate_fuctions_queries.sql

---
+++ Aggrigate_fuctions_queries.sql
@@ -0,0 +1,16 @@
+SELECT * FROM sales;
+
+--counts rows
+SELECT COUNT (*) FROM sales;
+
+--adds values
+SELECT SUM(quantity)FROM sales;
+
+--average value
+SELECT AVG(price) FROM sales;
+
+--smallest value
+SELECT MIN(price) FROM sales;
+
+--highest value
+SELECT MAX(price) FROM sales;

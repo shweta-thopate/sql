842d83e73e98d37d2e63dd134a9f3b79d30731bc
shweta-thopate <thopateshweta20@gmail.com>
2026-01-13 10:24:38 +0530

    Update select query.sql

--- select query.sql
+++ select query.sql
@@ -1,4 +1,9 @@
-select * from sales
-  
-select customer_name,price from sales	
-where customer_name='Sneha';
+SELECT * FROM sales;
+
+SELECT * FROM sales 
+where order_date='2026-01-13';
+--select the colums where order_date='2026-01-13';
+
+SELECT FROM sales
+WHERE product='Phone';
+--selects the customer_names where product='Phone';

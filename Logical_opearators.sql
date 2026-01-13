8435dae3f6ff40850e5cdc534d711b16ae6071be
shweta-thopate <thopateshweta20@gmail.com>
2026-01-13 17:46:20 +0530

    Update Logical_opearators.sql

--- Logical_opearators.sql
+++ Logical_opearators.sql
@@ -8,6 +8,6 @@ WHERE customer_name='Sneha' AND product='Phone';
 SELECT * FROM sales
 WHERE customer_name='Sneha' OR product='Phone';
 
---NOoperator
+--NOT operator
 SELECT * FROM sales
 WHERE NOT customer_name='Sneha' AND customer_name='shweta';
